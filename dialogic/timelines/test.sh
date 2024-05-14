d8903140e1e4258deea822f659120739db120232
benson0402 <benson930402@gmail.com>
2024-05-14 22:03:20 +0800

    Fix: name

--- dialogic/timelines/test.sh
+++ dialogic/timelines/test.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-sed -i -e 's/\[男主\]/文碩/g' *.json
-sed -i -e 's/文朔/文碩/g' *.json
+sed -i -e 's/文碩/文朔/g' *.json
 
 sed -i -e 's/\[正宮\]/小妤/g' *.json
 sed -i -e 's/曉杅/小妤/g' *.json
 sed -i -e 's/曉杅/小妤/g' *.json
 
 sed -i -e 's/\[朋1\]/小菜/g' *.json
+sed -i -e 's/\[朋一\]/小菜/g' *.json
 
 sed -i -e 's/\[朋2\]/小晴/g' *.json
+sed -i -e 's/\[朋二\]/小晴/g' *.json
 
 sed -i -e 's/\[學長\]/杰穎/g' *.json
