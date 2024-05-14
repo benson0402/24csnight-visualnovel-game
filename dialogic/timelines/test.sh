30a4dfc3b6e4369af62f6f8adb4aa6a211364d69
benson0402 <benson930402@gmail.com>
2024-05-14 20:51:37 +0800

    Feat: Adjust Character's name

---
+++ dialogic/timelines/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sed -i -e 's/\[男主\]/文碩/g' *.json
+sed -i -e 's/文朔/文碩/g' *.json
+
+sed -i -e 's/\[正宮\]/小妤/g' *.json
+sed -i -e 's/曉杅/小妤/g' *.json
+sed -i -e 's/曉杅/小妤/g' *.json
+
+sed -i -e 's/\[朋1\]/小菜/g' *.json
+
+sed -i -e 's/\[朋2\]/小晴/g' *.json
+
+sed -i -e 's/\[學長\]/杰穎/g' *.json
